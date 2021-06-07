2489b833bc1d2bdb886d9dd104993deaf82e2f67
Sheila Harder <56269957+Slipstitch@users.noreply.github.com>
2021-06-07 16:04:40 -0500

    Added repos to gallery

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-
+//global variables
 //where profile information will appear
 const profileOverview = document.querySelector (".overview");
 const username = "Slipstitch";
+//select unordered list to display the repos list
+const reposList = document.querySelector (".repo-list");
+
 
 //Fetch API JSON data
-
 const getUserInfo = async function () {
 	const showRequest = await fetch (`http://api.github.com/users/${username}`);
 	const data = await showRequest.json();
@@ -15,7 +17,6 @@ const getUserInfo = async function () {
 getUserInfo();
 
 //Fetch and Display User Information
-
 const displayUserInfo = function (data) {
 	const div = document.createElement("div");
 	div.classList.add("user-info");
@@ -30,5 +31,25 @@ const displayUserInfo = function (data) {
          <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
        </div>`;
        profileOverview.append(div);
+       getRepos();
+};
+
+//Fetch the repos
+const getRepos = async function () {
+	const fetchRepos = await fetch (`http://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+	const repoData = await fetchRepos.json();
+	displayRepoInfo(repoData);
+	
+};
+
+//Display information about each repo
+const displayRepoInfo = function (repos) {
+	for (const repo of repos) {
+		const repoItem = document.createElement ("li");
+		repoItem.classList.add ("repo");
+		repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+		reposList.append(repoItem);
+	}
+
 };
 
